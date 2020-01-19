6328ceacf395ce989aa93b03e6cfd24e5fe792c4
OverRatedTech <rchavezj.ai@gmail.com>
2020-01-19 15:30:11 -0800

    d

---
+++ src/components/yolo/yolo.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './card-list.styles.css';
+import { Card } from '../card/card.component';
+
+
+export const Yolo = (props) => {
+    return (
+        <div className='card-list'> {
+            props.monsters.map(
+                monster => (
+                    <Card key={monster.id} monster={monster} />
+                )
+            )
+        } </div>
+    );
+}
+
+
